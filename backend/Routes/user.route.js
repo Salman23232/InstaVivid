84aa7aea31546a72bb0b32240638c78a7423c3a6
Salman Ahmed <sscvocational4@gmail.com>
2024-12-25 12:12:04 +0600

    adding multer.js

--- backend/Routes/user.route.js
+++ backend/Routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getProfile, login, logout, register } from "../controllers/user.controller";
+import { editProfile, getProfile, login, logout, register } from "../controllers/user.controller";
 import isAuthenticated from "../middlewares/isAuthenticated";
+import upload from "../middlewares/multer";
 const router = express.Router();
 
 router.route('/register').post(register)
 router.route('/login').post(login)
 router.route('/logout').get(logout)
-router.route('/:id/profile').get(isAuthenticated, getProfile)
-router.route('/profile/edit').post(isAuthenticated)
+router.route('/:id/profile').get(isAuthenticated, getProfile) 
+router.route('/profile/edit').post(isAuthenticated,upload.single('profilePicture'), editProfile)
